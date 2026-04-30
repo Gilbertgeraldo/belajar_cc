27433ea836a6d83ee361d5b22166c91fef6a3fa5
Gilbertgeraldo <gilbertcat6@gmail.com>
2026-05-01 02:55:55 +0700

    e

---
+++ test.cc
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main() {
+//     cout << "------------------------" << endl;
+//     cout << "Projek kalkulator sederhana" << endl;
+//     cout << "------------------------" << endl;
+
+//     cout << "\nPILIHAN OPERASI KALKULATOR : " << endl;
+//     cout << "\t1.Penjumlahan" << endl;
+//     cout << "\t2.Pengurangan" << endl;
+//     cout << "\t3.Perkalian" << endl;
+//     cout << "\t4.Pembagian" << endl;
+
+//     int ch;
+//     cout << "\tPilihan anda (1/2/3/4) : ",cin >> ch;
+    
+//     double A,B;
+//     switch (ch) {
+//         case 1:
+//             cout << "\nMasukan bilangan A : ",cin >> A;
+//             cout << "\nMasukan bilangan B : ",cin >> B;
+//             cout << "\nHasil : " << A + B << endl;
+//             break;
+//         case 2:
+//             cout << "\nMasukan bilangan A : ",cin >> A;
+//             cout << "\nMasukan bilangan B : ",cin >> B;
+//             cout << "\nHasil : " << A - B << endl;
+//             break;
+//         case 3:
+//             cout << "\nMasukan bilangan A : ",cin >> A;
+//             cout << "\nMasukan bilangan B : ",cin >> B;
+//             cout << "\nHasil : " << A * B << endl;
+//             break;
+//         case 4:
+//             cout << "\nMasukan bilangan A : ",cin >> A;
+//             cout << "\nMasukan bilangan B : ",cin >> B;
+//             cout << "\nHasil : " << A / B << endl;
+//             break;
+//         default :
+//             cout << "\nError!" << endl;
+//     }
+//     return 0;
+// }
+
+void swapNums(int &x,int &y) {
+    int z = x;
+    int x = y;
+    int y = z;
+}
+
+int main() {
+    
+}
